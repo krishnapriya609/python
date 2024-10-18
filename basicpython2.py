a2adf0079e1c4903724bf3f28f7ce306b5eef22a
Krishnapriya <krishnapriyaar292@gmail.com>
2024-10-18 15:10:29 +0530

    Add files via upload

---
+++ basicpython2.py
@@ -0,0 +1,12 @@
+'''
+Author:krishnapriya
+date:18-10-2024
+Descripton:python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+'''
+str_var="Hello, World!"
+str_another_var=str_var[7:12]
+print(str_another_var)
+str_var1=" Everyone!"
+print(str_another_var+str_var1)
+
+
